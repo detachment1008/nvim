f4c488dab0e0078d088170d85e96631ab5d2ab37
dcr of ubuntu <974449413@qq.com>
2023-08-13 11:50:45 +0800

    change and not add
    Signed-off-by: dcr of ubuntu <974449413@qq.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,14 @@
-
 require "nvim-treesitter.configs".setup
 {
     -- 添加识别的语言
     ensure_installed = {"vim", "bash", "c", "cpp"--[[, "c_sharp"--]], "json", "lua"},
-
     -- 高亮
     highlight = { enable = false },
-
     -- 自动缩进
     indent = { enable = true },
-
-    -- 不同括号的颜色区分(似乎有问题，先禁用。它会错误的将某些括号变色，且无法自动变回来，只能重新输入)
-    -- 误会解除，是tokyonight的锅
-    -- 但是它会把括号颜色变成红色，奇怪
-    rainbow = {
+    --[[
+    rainbow =
+    {
         enable = true,
         -- disable = { "jsx", "cpp"}
         extended_mode = true,
@@ -27,5 +22,6 @@ require "nvim-treesitter.configs".setup
             '#FF00FF', -- 品红色
         },
     }
+    --]]
 }
 
