e9fb6c755221bc5a606d3665136f70107a56ab95
dengchaoran <dengchaoran@bolegames.com>
2023-11-09 21:05:44 +0800

    gitsigns 添加一些按键映射
    Signed-off-by: dengchaoran <dengchaoran@bolegames.com>

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -40,46 +40,46 @@ require('gitsigns').setup {
         enable = false
     },
 
-    -- on_attach = function(bufnr)
-    --     local gs = package.loaded.gitsigns
-    --
-    --     local function map(mode, l, r, opts)
-    --         opts = opts or {}
-    --         opts.buffer = bufnr
-    --         vim.keymap.set(mode, l, r, opts)
-    --     end
-    --
-    --     -- Navigation
-    --     map('n', ']c', function()
-    --         if vim.wo.diff then return ']c' end
-    --         vim.schedule(function() gs.next_hunk() end)
-    --         return '<Ignore>'
-    --     end, {expr=true})
-    --
-    --     map('n', '[c', function()
-    --         if vim.wo.diff then return '[c' end
-    --         vim.schedule(function() gs.prev_hunk() end)
-    --         return '<Ignore>'
-    --     end, {expr=true})
-    --
-    --     -- Actions
-    --     map('n', '<leader>hs', gs.stage_hunk)
-    --     map('n', '<leader>hr', gs.reset_hunk)
-    --     map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    --     map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    --     map('n', '<leader>hS', gs.stage_buffer)
-    --     map('n', '<leader>hu', gs.undo_stage_hunk)
-    --     map('n', '<leader>hR', gs.reset_buffer)
-    --     map('n', '<leader>hp', gs.preview_hunk)
-    --     map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-    --     map('n', '<leader>tb', gs.toggle_current_line_blame)
-    --     map('n', '<leader>hd', gs.diffthis)
-    --     map('n', '<leader>hD', function() gs.diffthis('~') end)
-    --     map('n', '<leader>td', gs.toggle_deleted)
-    --
-    --     -- Text object
-    --     map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-    -- end
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+        end, {expr=true})
+
+        map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+        end, {expr=true})
+
+        -- Actions
+        map('n', '<leader>gs', gs.stage_hunk) -- 当前块加入暂存区
+        map('n', '<leader>gS', gs.stage_buffer) -- 当前文件加入暂存区
+        map('n', '<leader>gr', gs.reset_hunk) -- 当前块恢复到上一次提交的状态
+        map('n', '<leader>gR', gs.reset_buffer) -- 当前文件恢复到上一次提交的状态
+        map('n', '<leader>gu', gs.undo_stage_hunk) -- 当前块取消暂存(似乎有 bug?)
+        map('n', '<leader>gp', gs.preview_hunk) -- 查看当前块的修改
+        map('n', '<leader>gb', function() gs.blame_line{full=true} end) -- 查看当前块最近一次提交的修改记录
+        -- map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>gd', gs.diffthis) -- 查看当前文件的修改记录: 左边是原始的，右边是修改后的
+        -- map('n', '<leader>hD', function() gs.diffthis('~') end) -- 似乎和上面的相同
+        -- map('n', '<leader>td', gs.toggle_deleted) -- 不知道是什么意思
+        map('v', '<leader>gs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end) -- 可视模式: 选中的加入暂存区
+        map('v', '<leader>gr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end) -- 可视模式: 选中的恢复到上一次提交的状态
+
+        -- Text object
+        map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    end
 }
 
 vim.cmd(":Gitsigns toggle_current_line_blame")
