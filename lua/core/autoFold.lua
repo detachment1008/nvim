fe175b2107beca3d31cc2e5fcd7e63e044c9ce29
dengchaoran <dengchaoran@bolegames.com>
2023-11-16 18:03:09 +0800

    优化性能
    Signed-off-by: dengchaoran <dengchaoran@bolegames.com>

--- lua/core/autoFold.lua
+++ lua/core/autoFold.lua
@@ -1,7 +1,7 @@
 local autoImpl = require "core.autoImpl"
 
 local autoFold = {}
-local flag = false
+local flag = true
 function autoFold:ToggleFold()
     flag = not flag
     local pattern1 = "^function"
@@ -9,16 +9,17 @@ function autoFold:ToggleFold()
     local path = vim.fn.expand('%')
     local file_contents = vim.fn.readfile(path)
     vim.cmd("normal zE")
+    if flag then
+        return
+    end
     for i,line in ipairs(file_contents) do
-        if flag then
-            local result = string.match(line, pattern1) or string.match(line, pattern2)
-            if result then
-                vim.cmd("normal " .. i .. "G")
-                vim.cmd("normal V")
-                local end_line = autoImpl:FindFunctionEnd(file_contents, i)
-                vim.cmd("normal " .. end_line .. "G")
-                vim.cmd("normal zf")
-            end
+        local result = string.match(line, pattern1) or string.match(line, pattern2)
+        if result then
+            vim.cmd("normal " .. i .. "G")
+            vim.cmd("normal V")
+            local end_line = autoImpl:FindFunctionEnd(file_contents, i)
+            vim.cmd("normal " .. end_line .. "G")
+            vim.cmd("normal zf")
         end
     end
 end
