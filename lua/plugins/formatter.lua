f640bbf17fde87bfa956032ab3fda953e264086d
dcr <974449413@qq.com>
2023-12-21 20:14:50 +0800

    revert
    Signed-off-by: dcr <974449413@qq.com>

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -55,7 +55,7 @@ require("formatter").setup({
 vim.cmd([[
     augroup FormatAutogroup
         autocmd!
-        autocmd BufWritePost *.c *.cpp *.h FormatWrite
+        autocmd BufWritePost * FormatWrite
     augroup END
 ]])
 -- vim.cmd([[
